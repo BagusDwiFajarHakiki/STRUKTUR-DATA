80f2b65e171a2eddae67d223148542175ff55389
BagusDwi <114036813+BagusDwiFajarHakiki@users.noreply.github.com>
2024-03-05 22:52:18 +0700

    Add files via upload

---
+++ Pertemuan-2/src/PERULANGAN/forloop.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package PERULANGAN;
+
+/**
+ *
+ * @author ASUS
+ */
+public class forloop 
+{
+    public static void main(String[] args) 
+    {
+        for (int i = 1; i < 10; i++) {
+        System.out.println("angka ke-" + i);
+    }
+  }
+    
+}
